45c07aa0fcef2ac85e3b1caf8e7eb219cc37a4ae
Sukhvindersingh1406 <147186353+Sukhvindersingh1406@users.noreply.github.com>
2023-10-19 21:32:03 +0530

    Add files via upload

---
+++ suk.c
@@ -0,0 +1,20 @@
+//if else to find vowel or consonant for given character
+
+#include<stdio.h>
+int main()
+{
+    char x;
+    printf("Enter the char you want to check for vowel and consonant \n");
+    scanf("%c",&x);
+
+    if(x=='a' || x=='e' || x=='i' || x=='o' || x=='u' || x=='A' || x=='E' || x=='I' || x=='O' || x=='U')
+    {
+        printf("The given character is vowel\n");
+    }
+    else
+    {
+        printf("The given character is consonant\n");
+    }
+
+    return 0;
+}
