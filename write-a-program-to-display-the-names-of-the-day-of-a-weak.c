26f4003fcb05bf6e86dbffd97bb0b6b30a847a96
Sukhvindersingh1406 <147186353+Sukhvindersingh1406@users.noreply.github.com>
2023-10-31 22:51:07 +0530

    Add files via upload

---
+++ write-a-program-to-display-the-names-of-the-day-of-a-weak.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int day;
+    printf("enter the number between 1 to 7");
+    scanf("%d",&day);
+    switch(day)
+    {
+        case1:
+        printf("\n day 1 is sunday");
+        break;
+        
+        case2:
+        printf("\n day 2 is monday");
+        break;
+
+        case3:
+        printf("\n day 3 is tuesday");
+        break;
+
+        case4:
+        printf("\n day4 is wednesday");
+        break;
+
+        case5 :
+        printf("\n day 5 is thursday");
+        break;
+
+        case6 :
+        printf("\n day 6 is friday");
+        break;
+
+        case7:
+        printf("\n day 7 is saturday");
+        break;
+
+         return 0;
+
+    }
+}
